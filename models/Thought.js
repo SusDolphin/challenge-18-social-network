77ffc0e37ba13a46b2e253b9d2c4717184c30d92
Larry Fletcher <susdolphin@Larrys-Laptop.local>
2025-01-19 15:03:32 -0500

    thoughts.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,27 +1,30 @@
 const { Schema, model } = require('mongoose');
 const Reaction = require('./Reaction');
 
-// Schema to create Post model
 const thoughtSchema = new Schema(
   {
     thoughtText: {
       type: String,
-      required:true,
+      required: true,
       minLength: 1,
       maxLength: 280,
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get:(date)=>{
-        return date.toLocaleDateString() //built-in date converter to display MM/DD/YYYY
-      }
+      get: (date) => {
+        return date.toLocaleString()
+      },
     },
     username: {
       type: String,
-     required: true,
+      require: true,
     },
-    
+    description: {
+      type: String,
+      minLength: 8,
+      maxLength: 500,
+    },
     reactions: [Reaction],
   },
   {
@@ -32,15 +35,12 @@ const thoughtSchema = new Schema(
   }
 );
 
-// Create a virtual property `responses` that gets the amount of response per video
 thoughtSchema
   .virtual('reactionCount')
-  // Getter
   .get(function () {
     return this.reactions.length;
   });
 
-// Initialize our Video model
 const Thought = model('thought', thoughtSchema);
 
 module.exports = Thought;
